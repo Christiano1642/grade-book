99ee96693b0c884338cbd1145c8c541087e619e2
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-08 17:48:55 -0600

    password created

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,11 +1,19 @@
+import bcrypt from "bcrypt";
 import client from "../client.js";
 import config from "../config.js";
 
 const admin = client.db(config.db.name).collection("admin");
 
 export default {
-  create(username, password) {
-    // Check for an existing user in the db
-    console.log(username, password, "from admin controller");
+  async create(username, password) {
+    const existingUser = admin.findOne({ username });
+
+    if (existingUser) {
+      throw new Error(`User ${username} already exists`);
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    return admin.insertOne({ username, password: hash });
   },
 };
