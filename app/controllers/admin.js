32a8f6eed927c0668c776a4ff175c66e2176494f
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-11 12:55:54 -0600

    created admin with jwt, create and show methods

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,19 +1,38 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client.js";
 import config from "../config.js";
 
 const admin = client.db(config.db.name).collection("admin");
 
 export default {
-  async create(username, password) {
-    const existingUser = admin.findOne({ username });
+  async create({ username, password, role }) {
+    const existingUser = await admin.findOne({ username });
 
     if (existingUser) {
       throw new Error(`User ${username} already exists`);
     }
 
-    const hash = await bcrypt.hash(password, 10);
+    const hash = await bcrypt.hash(password, config.encryption.saltRounds);
 
-    return admin.insertOne({ username, password: hash });
+    return admin.insertOne({ username, password: hash, role });
+  },
+
+  async show({ username, password }) {
+    const user = await admin.findOne({ username });
+
+    if (!user) {
+      throw new Error(`User ${username} not found`);
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      throw Error("Password does not match");
+    }
+
+    return jwt.sign({ username, role: user.role }, config.encryption.secret, {
+      expiresIn: config.encryption.expiration,
+    });
   },
 };
