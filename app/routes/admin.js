3288a467717245d3a87032dcbab4712fc23aaa8f
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-11 12:59:16 -0600

    Update admin route with new admin class

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,18 +1,29 @@
 import { Router } from "express";
 import adminController from "../controllers/admin.js";
+import Admin from "../models/users/Admin.js";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
-  res.send("Hello World!");
+  res.send("Hello Admin!");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   try {
-  const { username, password } = req;
-  const newUser = await adminController.create(username, password);
-  res.json(newUser);
-  console.log(req.body.controllers(username, password));}
-  catch(err) res.status(400).json({message: err.message});)}
+    const admin = new Admin(req.body);
+
+    const errors = admin.validate();
+    if (errors.length) {
+      throw new Error(errors.join("\n"));
+    }
+    console.log(admin);
+    await adminController.create(admin);
+    const jwt = await adminController.show(admin);
+
+    res.status(201).json(jwt);
+  } catch ({ message }) {
+    res.status(400).json({ message });
+  }
+});
 
 export default router;
