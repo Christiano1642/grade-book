3434e81b48a7a0631c08b9fb9a35c367c887fffc
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-11 13:02:16 -0600

    added student route with auth

--- app/routes/students.js
+++ app/routes/students.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import studentsController from "../controllers/students.js";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+router.post("/students", async (req, res) => {
+  if (req.isAuth) {
+    const students = await studentsController.index();
+    res.send(students);
+  }
 });
 
 export default router;
