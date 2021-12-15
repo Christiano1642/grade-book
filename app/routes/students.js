dece90bfb43ac42c6e88d3ea1a16c7b18af1ad7a
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-15 14:41:35 -0600

    Added routes to show and update students

--- app/routes/students.js
+++ app/routes/students.js
@@ -20,4 +20,27 @@ router.post("/alumni", async ({ isAuth }, res) => {
   }
 });
 
+router.post("/:id", async ({ isAuth, params }, res) => {
+  if (isAuth === "ADMIN") {
+    try {
+      const student = await studentsController.show(params.id);
+      res.json(student);
+    } catch ({ message }) {
+      res.status(500).json({ message });
+    }
+  } else {
+    res.status(401).json({ message: "Acces Denied" });
+  }
+});
+
+router.put("/grade/:id", async ({ isAuth, body, params }, res) => {
+  if (isAuth.role === "ADMIN") {
+    try {
+      const update = await studentsController.update(params.id, body);
+      res.json(update);
+    } catch ({ message }) {
+      res.status(500).send({ message });
+    }
+  }
+});
 export default router;
