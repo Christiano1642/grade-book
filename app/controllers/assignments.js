08754ed53b4ae8545bac1c197e639b4225712e5b
Christian Wallace <christian.wallace@student.swic.edu>
2021-12-15 14:40:44 -0600

    Create assignment controller

--- app/controllers/assignments.js
+++ app/controllers/assignments.js
@@ -2,10 +2,12 @@ import client from "../client.js";
 import config from "../config.js";
 
 const assignment = client.db(config.db.name).collection("assignment");
+const student = client.db(config.db.name).collection("student");
 
 export default {
   async create(newAssignment) {
-    const result = await assignment.insertOne(newAssignment);
-    return result;
+    const { insertedID } = await assignment.insertOne(newAssignment);
+    await student.updateOne({}, { $push: { grades: newAssignment } });
+    return insertedID;
   },
 };
